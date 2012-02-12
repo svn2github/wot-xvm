694a3f7665367c823d5897a9f1a6cd1e70b3cab0
m.schedriviy <m.schedriviy@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2012-02-12 16:32:35 +0000

    fixed deploy script
    git-svn-id: http://wot-xvm.googlecode.com/svn@44 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/utils/deploy.sh
+++ trunk/utils/deploy.sh
@@ -3,7 +3,6 @@
 cd $(dirname $(realpath $0))
 
 [ "$WOT_DIRECTORY" = "" ] && WOT_DIRECTORY=/cygdrive/d/home/games/WoT
-WOT_DIRECTORY=/cygdrive/d/home/games/WoT
 cp ../bin/*.swf ${WOT_DIRECTORY}/res/gui/flash
 cp ../bin/XVM.xvmconf ${WOT_DIRECTORY}/res/gui/flash/XVM.xvmconf
 cp ../bin/OTMData.xml ${WOT_DIRECTORY}/res/gui/flash/OTMData.xml
