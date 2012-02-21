f4a36ce3f597284ddbeb3c35b8de2d7cdd039462
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2012-02-21 10:04:46 +0000

    * sorting replays * added replay with fog of war
    git-svn-id: http://wot-xvm.googlecode.com/svn@95 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/utils/test.sh
+++ trunk/utils/test.sh
@@ -6,10 +6,13 @@ cd $(dirname $(realpath $(cygpath --unix $0)))
 
 [ "$WOT_DIRECTORY" = "" ] && WOT_DIRECTORY=/cygdrive/d/home/games/WoT
 CURRENT_DIRECTORY=`pwd`
-#SAMPLE_REPLAY=1.wotreplay
-SAMPLE_REPLAY=blowup.wotreplay
-#SAMPLE_REPLAY=tk.wotreplay
+
+#SAMPLE_REPLAY=15.wotreplay
+#SAMPLE_REPLAY=fogofwar.wotreplay
+#SAMPLE_REPLAY=markers.wotreplay
 #SAMPLE_REPLAY=squad.wotreplay
+SAMPLE_REPLAY=tk,blowup.wotreplay
+
 cd ${WOT_DIRECTORY}
 #cmd /c start ./WorldOfTanks.exe `cygpath --windows ${CURRENT_DIRECTORY}/../test/replays/${SAMPLE_REPLAY}`
 cmd /c start ./wot-xvm-proxy.exe /debug `cygpath --windows ${CURRENT_DIRECTORY}/../test/replays/${SAMPLE_REPLAY}`
