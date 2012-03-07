7588469ccb1e2de5ae1b64ec582166cc8963a6ba
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2012-03-07 10:28:29 +0000

    utils updated
    git-svn-id: http://wot-xvm.googlecode.com/svn@161 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/utils/deploy.sh
+++ trunk/utils/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-cd $(dirname $(realpath $0))
+GAME_VER=0.7.1.1
+
+cd $(dirname $(realpath $(cygpath --unix $0)))
 
 [ "$WOT_DIRECTORY" = "" ] && WOT_DIRECTORY=/cygdrive/d/home/games/WoT
-cp ../bin/*.swf ${WOT_DIRECTORY}/res/gui/flash
-cp ../bin/XVM.xvmconf ${WOT_DIRECTORY}/res/gui/flash/XVM.xvmconf
-cp ../bin/OTMData.xml ${WOT_DIRECTORY}/res/gui/flash/OTMData.xml
+SWF_DIR=$WOT_DIRECTORY/res_mods/$GAME_VER/gui/flash
+[ -f "../bin/*.swf" ] && cp ../bin/*.swf $SWF_DIR
+[ -f "../bin/XVM.xvmconf" ] && cp ../bin/XVM.xvmconf $SWF_DIR/XVM.xvmconf
+[ -f "../bin/OTMData.xml" ] && cp ../bin/OTMData.xml $SWF_DIR/OTMData.xml
