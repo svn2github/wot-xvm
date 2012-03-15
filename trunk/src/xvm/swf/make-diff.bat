a81678def9f93ad0a508a08083d06771a38264d3
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2012-03-15 15:24:20 +0000

    swf changed to WoT 0.7.2-test2
    git-svn-id: http://wot-xvm.googlecode.com/svn@208 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/src/xvm/swf/make-diff.bat
+++ trunk/src/xvm/swf/make-diff.bat
@@ -1,9 +1,7 @@
 @echo off
 
-set VER=0.7.2-test1
-
-diff -u battle-%VER%.xml battle.xml > battle.xml.patch
-diff -u battleloading-%VER%.xml battleloading.xml > battleloading.xml.patch
-diff -u PlayersPanel-%VER%.xml PlayersPanel.xml > PlayersPanel.xml.patch
-rem diff -u StatisticForm-%VER%.xml StatisticForm.xml > StatisticForm.xml.patch
-diff -u VehicleMarkersManager-%VER%.xml VehicleMarkersManager.xml > VehicleMarkersManager.xml.patch
+diff -u orig\battle.xml battle.xml > battle.xml.patch
+diff -u orig\battleloading.xml battleloading.xml > battleloading.xml.patch
+diff -u orig\PlayersPanel.xml PlayersPanel.xml > PlayersPanel.xml.patch
+diff -u orig\StatisticForm.xml StatisticForm.xml > StatisticForm.xml.patch
+diff -u orig\VehicleMarkersManager.xml VehicleMarkersManager.xml > VehicleMarkersManager.xml.patch
--- trunk/src/xvm/swf/make-swf.bat
+++ trunk/src/xvm/swf/make-swf.bat
@@ -1,6 +1,7 @@
 @echo off
+
 swfmill xml2swf battle.xml battle.swf
 swfmill xml2swf battleloading.xml battleloading.swf
 swfmill xml2swf PlayersPanel.xml PlayersPanel.swf
-rem swfmill xml2swf StatisticForm.xml StatisticForm.swf
+swfmill xml2swf StatisticForm.xml StatisticForm.swf
 swfmill xml2swf VehicleMarkersManager.xml VehicleMarkersManager.swf
