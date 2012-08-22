4fa629394354ac224df8342b78ce38d105f1e254
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2012-08-22 12:27:05 +0000

    Update swf's for 0.8.0.CT1.
    git-svn-id: http://wot-xvm.googlecode.com/svn@644 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- branches/WoT-0.8-test/src/xvm/swf/make-swf.bat
+++ branches/WoT-0.8-test/src/xvm/swf/make-swf.bat
@@ -1,8 +1,28 @@
 @echo off
 
-swfmill xml2swf battle.xml battle.swf
-swfmill xml2swf battleloading.xml battleloading.swf
-swfmill xml2swf PlayersPanel.xml PlayersPanel.swf
-swfmill xml2swf StatisticForm.xml StatisticForm.swf
-rem swfmill xml2swf FinalStatisticForm.xml FinalStatisticForm.swf
-swfmill xml2swf VehicleMarkersManager.xml VehicleMarkersManager.swf
+set cleanup=1
+::set cleanup=0
+
+set names=battle battleloading PlayersPanel StatisticForm VehicleMarkersManager
+
+for %%i in (%names%) do (
+  echo =^> %%i
+
+  rem decode original swf
+  swfmill swf2xml orig\%%i.swf orig\%%i.xml
+
+  rem copy original xml
+  copy /Y orig\%%i.xml %%i.xml > nul
+
+  rem patch xml
+  patch < %%i.xml.patch > nul
+
+  rem build patched swf
+  swfmill xml2swf %%i.xml patched\%%i.swf
+
+  rem cleanup
+  if "%cleanup%" == "1" (
+    del orig\%%i.xml
+    del %%i.xml
+  )
+)
