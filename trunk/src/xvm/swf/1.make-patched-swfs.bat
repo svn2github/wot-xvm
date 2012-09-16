cd92e15c523945655bd3bc358fc7f1bbdccab33c
m.schedriviy <m.schedriviy@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2012-09-16 14:19:31 +0000

    do not patch battle.swf (only main entry point is used)
    git-svn-id: http://wot-xvm.googlecode.com/svn@742 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/src/xvm/swf/1.make-patched-swfs.bat
+++ trunk/src/xvm/swf/1.make-patched-swfs.bat
@@ -1,12 +1,17 @@
 @echo off
 
-set swfs=battle.swf battleloading.swf PlayersPanel.swf StatisticForm.swf VehicleMarkersManager.swf
+set patch_swfs=battleloading.swf PlayersPanel.swf StatisticForm.swf VehicleMarkersManager.swf
+set copy_swfs=battle.swf
 
-for %%i in (%swfs%) do call :do_file %%~ni
+rem Copy SWFs without patching
+for %%i in (%copy_swfs%) do call :do_copy_file %%~ni
+
+rem Patch SWFs
+for %%i in (%patch_swfs%) do call :do_patch_file %%~ni
 
 goto :EOF
 
-:do_file
+:do_patch_file
 set n=%1
 swfmill swf2xml orig\%n%.swf orig\%n%.xml
 copy /Y orig\%n%.xml %n%.xml > nul
@@ -18,4 +23,9 @@ if "%ok%" == "ok" (
   swfmill xml2swf %n%.xml %n%.swf
   del %n%.xml orig\%n%.xml
 )
+goto :EOF
+
+:do_copy_file
+echo copying file %1.swf
+copy /Y orig\%1.swf %1.swf > nul
 goto :EOF
