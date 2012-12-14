cfe0cf53b9cb6510304054aff4d76c93bb06b9ba
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2012-12-14 16:08:47 +0000

    update proxy scripts
    git-svn-id: http://wot-xvm.googlecode.com/svn@1169 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/src/nodejs-xvm-proxy/bin/calc/export-csv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-port=27017
-
-mongo --port $port --quiet xvm export-csv.js > vstat.csv
---
+++ trunk/src/nodejs-xvm-proxy/bin/calc/export-xml.js
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+var doExport = function(x){
+    if (x.region != "RU")
+        return;
+    var d = 0, ed = 0, f = 0, s = 0;
+    for (var i in x.topD)
+        d += x.topD[i];
+    d /= x.topD.length;
+    for (var i in x.topED)
+        ed += x.topED[i];
+    ed /= x.topED.length;
+    for (var i in x.topF)
+        f += x.topF[i];
+    f /= x.topF.length;
+    for (var i in x.topS)
+        s += x.topS[i];
+    s /= x.topS.length;
+    print("<v>" +
+        "<vname>" + x.vname.toLowerCase() + "</vname>" +
+        "<hp>" + x.hp + "</hp>" +
+        "<b>" + x.b + "</b>" +
+        "<w>" + x.w + "</w>" +
+        "<avgD>" + (x.d / x.b).toFixed(0) + "</avgD>" +
+        "<avgED>" + (x.d / x.b / x.hp).toFixed(2) + "</avgED>" +
+        "<avgF>" + (x.f / x.b).toFixed(2) + "</avgF>" +
+        "<avgS>" + (x.s / x.b).toFixed(2) + "</avgS>" +
+        "<topD>" + d.toFixed(0) + "</topD>" +
+        "<topED>" + ed.toFixed(2) + "</topED>" +
+        "<topF>" + f.toFixed(2) + "</topF>" +
+        "<topS>" + s.toFixed(2) + "</topS>" +
+        "</v>");
+}
+
+print("<vstat>");
+db.vstat.find({}, {_id:0}).forEach(doExport);
+print("</vstat>");
