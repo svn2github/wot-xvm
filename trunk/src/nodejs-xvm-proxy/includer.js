9dd33c27deda583381a115264b7aa2b7062baa88
batsvr@gmail.com <batsvr@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-02-14 22:23:32 +0000

    fix
    git-svn-id: http://wot-xvm.googlecode.com/svn@1401 802240b0-0b4d-18ce-4811-9324b01ec0ff

---
+++ trunk/src/nodejs-xvm-proxy/includer.js
@@ -0,0 +1,116 @@
+module.exports = /*{
+    http: require("http"),
+    async: require("async"),
+    utils: require("./utils"),
+    settings: require("./settings").settings,
+    status: require("./worker_status"),
+    db: require("./worker_db"),
+    req: require("./worker_req"),
+    tcalc: require("./tcalc/tcalc"),
+    tcalcBase: require("./tcalc/tcalc_base")
+};*/
+
+
+
+(function() {
+    var http;
+    this.http = function() {
+        if(!http) {
+            http = require("http");
+        }
+        return http;
+    };
+    this.setHttp = function(newHttp) {
+        http = newHttp;
+    };
+
+    var async;
+    this.async = function() {
+        if(!async) {
+            async = require("async");
+        }
+        return async;
+    };
+
+    var utils;
+    this.utils = function() {
+        if(!utils) {
+            utils = require("./utils");
+        }
+        return utils;
+    };
+
+    var settings;
+    this.settings = function() {
+        if(!settings) {
+            settings = require("./settings").settings;
+        }
+        return settings;
+    };
+
+    var status;
+    this.status = function() {
+        if(!status) {
+            status = require("./worker_status");
+        }
+        return status;
+    };
+
+    var db;
+    this.db = function() {
+        if(!db) {
+            db = require("./worker_db");
+        }
+        return db;
+    };
+
+    var get;
+    this.get = function() {
+        if(!get) {
+            get = require("./worker_get");
+        }
+        return get;
+    };
+
+    var req;
+    this.req = function() {
+        if(!req) {
+            req = require("./worker_req");
+        }
+        return req;
+    };
+
+    var cmd;
+    this.cmd = function() {
+        if(!cmd) {
+            cmd = require("./worker_cmd");
+        }
+        return cmd;
+    };
+
+    var url;
+    this.url = function() {
+        if(!url) {
+            url = require("url");
+        }
+        return url;
+    };
+
+    var tcalc;
+    this.tcalc = function() {
+        if(!tcalc) {
+            tcalc = require("./tcalc/tcalc");
+        }
+        return tcalc;
+    };
+
+    var tcalcBase;
+    this.tcalcBase = function() {
+        if(!tcalcBase) {
+            tcalcBase = require("./tcalc/tcalc_base");
+        }
+        return tcalcBase;
+    };
+
+    return this;
+})();
