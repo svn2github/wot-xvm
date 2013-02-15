22602bae4ec2cd76c6e8f2ca0eeadaae0733c7f4
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-02-15 08:04:44 +0000

    SWFs for WoT 0.8.4
    git-svn-id: http://wot-xvm.googlecode.com/svn@1404 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/src/xvm/swf/make-diff.bat
+++ trunk/src/xvm/swf/make-diff.bat
@@ -12,7 +12,7 @@ if not exist %n%.xml (
   echo %n%.swf
   swfmill swf2xml orig\%n%.swf orig\%n%.xml
   swfmill swf2xml %n%.swf %n%.xml
-  diff -u orig\%n%.xml %n%.xml > %n%.xml.patch
+  diff -u -I "<StackDouble value=" orig\%n%.xml %n%.xml > %n%.xml.patch
   del orig\%n%.xml %n%.xml
 )
 goto :EOF
