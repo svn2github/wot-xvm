ca1e1e0e3542eaffaf1e70195b6cae9ab045e67c
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-04-05 19:01:57 +0000

    adaptation to WoT 0.8.5.CT1
    git-svn-id: http://wot-xvm.googlecode.com/svn@1636 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/utils/deploy.sh
+++ trunk/utils/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-[ "$GAME_VER" = "" ] && GAME_VER="0.8.4"
-#[ "$GAME_VER" = "" ] && GAME_VER="0.8.4 Common Test"
-[ "$WOT_DIRECTORY" = "" ] && WOT_DIRECTORY=/cygdrive/d/work/games/WoT
+#[ "$GAME_VER" = "" ] && GAME_VER="0.8.4"
+[ "$GAME_VER" = "" ] && GAME_VER="0.8.5 Common Test"
+[ "$WOT_DIRECTORY" = "" ] && WOT_DIRECTORY=/cygdrive/d/work/games/WoT-CT
 
 FILES="
   battle.swf
--- trunk/utils/test.sh
+++ trunk/utils/test.sh
@@ -4,7 +4,7 @@ cd $(dirname $(realpath $(cygpath --unix $0)))
 
 ./deploy.sh
 
-[ "$WOT_DIRECTORY" = "" ] && WOT_DIRECTORY=/cygdrive/d/work/games/WoT
+[ "$WOT_DIRECTORY" = "" ] && WOT_DIRECTORY=/cygdrive/d/work/games/WoT-CT
 CURRENT_DIRECTORY=`pwd`
 
 #SAMPLE_REPLAY=cap.wotreplay
