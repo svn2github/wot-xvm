f497b9c9fb5de73321e5d4fd168d493cc2c230ea
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-05-05 20:29:10 +0000

    xvm-stat version bump
    git-svn-id: http://wot-xvm.googlecode.com/svn@1798 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/src/xvm-stat/Properties/AssemblyInfo.cs
+++ trunk/src/xvm-stat/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 // XVM Stat version
-[assembly: AssemblyVersion("1.4.3.*")]
-[assembly: AssemblyFileVersion("1.4.3.*")]
+[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyFileVersion("1.5.0.*")]
 // XVM Mod version
-[assembly: AssemblyInformationalVersion("3.5.0")]
+[assembly: AssemblyInformationalVersion("4.0.0")]
