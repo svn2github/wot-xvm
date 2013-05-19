4fbd7d2aa281981f3938de2bf4b89e5c2bcbb247
mr.a.one94@gmail.com <mr.a.one94@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-05-19 10:19:55 +0000

    clanicons upd * rating for EU: http://wotclans.net/eu/showclansrating
    git-svn-id: http://wot-xvm.googlecode.com/svn@1850 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/addons/clanicons/update-topclans-eu.sh
+++ trunk/addons/clanicons/update-topclans-eu.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-url=""
+url="http://wotclans.net/eu/showclansrating/"
 host=worldoftanks.eu
 dir=EU
 top_count=150
 topfile=topclans-eu.txt
-#topfile_persist=topclans-eu.persist.txt
+topfile_persist=topclans-eu.persist.txt
+
+# Cleaning top clans file
+echo -n > $topfile
 
 . .update-topclans.sh
