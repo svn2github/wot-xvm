622f23649b276c5d60908c04af142338be814102
pavlushko.m@gmail.com <pavlushko.m@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-08-04 20:14:52 +0000

    build-linux: work around static linking in xbuild
    git-svn-id: http://wot-xvm.googlecode.com/svn@2279 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/utils/build-system-linux/sh/xvmstat-build.sh
+++ trunk/utils/build-system-linux/sh/xvmstat-build.sh
@@ -9,4 +9,5 @@ rm -rf ../../../src/xvm-stat/obj/*
 
 pushd ../../../src/xvm-stat/ > /dev/null
 /opt/mono-3/bin/xbuild xvm-stat.csproj /p:Configuration="Release" /p:TargetFrameworkProfile="" /p:SignAssembly="False" /p:win32icon="WoT.ico" /verbosity:quiet
+cp SQLite/Community.CsharpSqlite.dll bin/Release/ #FIXME: google about xbuild and static linking
 popd > /dev/null
