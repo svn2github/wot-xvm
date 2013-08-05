852568f95c088acbc509f4b92ce157e4c6cafb21
pavlushko.m@gmail.com <pavlushko.m@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-08-05 07:09:39 +0000

    build-linux: switch back to mono 2.10
    git-svn-id: http://wot-xvm.googlecode.com/svn@2280 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/utils/build-system-linux/sh/xvmstat-build.sh
+++ trunk/utils/build-system-linux/sh/xvmstat-build.sh
@@ -8,6 +8,6 @@ rm -rf ../../../src/xvm-stat/bin/*
 rm -rf ../../../src/xvm-stat/obj/*
 
 pushd ../../../src/xvm-stat/ > /dev/null
-/opt/mono-3/bin/xbuild xvm-stat.csproj /p:Configuration="Release" /p:TargetFrameworkProfile="" /p:SignAssembly="False" /p:win32icon="WoT.ico" /verbosity:quiet
+xbuild xvm-stat.csproj /p:Configuration="Release" /p:TargetFrameworkProfile="" /p:SignAssembly="False" /p:win32icon="WoT.ico" /verbosity:quiet
 cp SQLite/Community.CsharpSqlite.dll bin/Release/ #FIXME: google about xbuild and static linking
 popd > /dev/null
