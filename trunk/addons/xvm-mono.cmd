e5cb1f16e4a75dde1db1fa2faa003c04c77fd455
pavlushko.m@gmail.com <pavlushko.m@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-08-19 15:15:34 +0000

    addons: fix mono bat file. Thanks to Seriych.
    git-svn-id: http://wot-xvm.googlecode.com/svn@2332 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/addons/xvm-mono.cmd
+++ trunk/addons/xvm-mono.cmd
@@ -1,2 +1,6 @@
-SET MONO=C:\Program Files\Mono-3.0.10\bin
+set PFiles=%ProgramFiles%
+if DEFINED ProgramFiles(x86) set PFiles=%ProgramFiles(x86)%
+
+SET MONO=%PFiles%\Mono-3.0.10\bin
+
 "%MONO%\mono.exe" xvm-stat.exe
