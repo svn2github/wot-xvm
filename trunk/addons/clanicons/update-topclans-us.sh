53f7e79b2473ddd1931287c1354c4d6575541ef8
mr.a.one94@gmail.com <mr.a.one94@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-09-02 19:31:19 +0000

    NA clanrating http://wotclans.net/na/showclansrating
    git-svn-id: http://wot-xvm.googlecode.com/svn@2443 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/addons/clanicons/update-topclans-us.sh
+++ trunk/addons/clanicons/update-topclans-us.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-parser=null
-url=""
+parser=ivanerr
+url="http://wotclans.net/na/showclansrating/"
 host=worldoftanks.com
 dir=US
 top_count=150
 topfile=topclans-us.txt
 topfile_persist=topclans-us.persist.txt
+
+# Cleaning top clans file
+echo -n > $topfile
 
 . .update-topclans.sh
