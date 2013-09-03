db776708db45b66fe569a1cc69bb589ebaa73a77
malyshkirill <malyshkirill@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-09-03 14:04:22 +0000

    XPM: modified scripts for building SWC on Win x64
    git-svn-id: http://wot-xvm.googlecode.com/svn@2451 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- branches/xpm/src/xvm/build-swc-wg.sh
+++ branches/xpm/src/xvm/build-swc-wg.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+    PROGRAMFILESPATH="$PROGRAMFILES (x86)"
+else
+    PROGRAMFILESPATH="$PROGRAMFILES"
+fi
+
 if [ "$OS" = "Windows_NT" ]; then
     if [ "$FLEXSDK" = "" ]; then
-        FLEXSDK="$PROGRAMFILES/FlashDevelop/Tools/flexsdk"
+        FLEXSDK="$PROGRAMFILESPATH/FlashDevelop/Tools/flexsdk"
         compc="$FLEXSDK/bin/compc.exe"
     else
         compc="$FLEXSDK/bin/compc" #Apache Flex SDK has only bat and shell scripts
