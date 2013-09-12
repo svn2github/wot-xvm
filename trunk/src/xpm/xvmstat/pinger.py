50cf38d92d09e2efab3567e5619fab97e41645e1
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-09-12 14:13:24 +0000

    fix ping
    git-svn-id: http://wot-xvm.googlecode.com/svn@2572 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/src/xpm/xvmstat/pinger.py
+++ trunk/src/xpm/xvmstat/pinger.py
@@ -14,10 +14,9 @@ import threading
 import os
 import re
 from subprocess import Popen, PIPE, STARTUPINFO, STARTF_USESHOWWINDOW, SW_HIDE
-from Queue import Queue
 
 import BigWorld
-from predefined_hosts import g_preDefinedHosts
+import ResMgr
 
 from logger import *
 from constants import *
@@ -35,8 +34,15 @@ class _Ping(object):
         self.lock = threading.RLock()
         self.thread = None
         self.resp = None
-        pass
 
+        self.hosts = list()
+        loginSection = ResMgr.openSection('scripts_config.xml')['login']
+        log(str(loginSection))
+        if loginSection is not None:
+            for (name, subSec) in loginSection.items():
+                self.hosts.append({
+                    'name':subSec.readStrings('name')[0],
+                    'url':subSec.readStrings('url')[0]})
 
     def ping(self, proxy):
         if proxy not in self.listeners:
@@ -47,10 +53,6 @@ class _Ping(object):
         self._ping()
 
     def _ping(self):
-        # wait for hosts initialization
-        if not g_preDefinedHosts._hosts:
-            BigWorld.callback(0.05, self._ping)
-            return
         # create thread
         self.thread = threading.Thread(target=self._pingAsync)
         self.thread.daemon = True
@@ -102,31 +104,31 @@ class _Ping(object):
             processes = dict()
 
             # Ping all servers in parallel
-            for x in g_preDefinedHosts._hosts:
-                processes[x.name] = Popen(args + x.url.split(':')[0], stdout=PIPE, env=env, startupinfo=si)
+            for x in self.hosts:
+                processes[x['name']] = Popen(args + x['url'].split(':')[0], stdout=PIPE, env=env, startupinfo=si)
 
             # Parse ping output
-            for x in g_preDefinedHosts._hosts:
-                proc = processes[x.name]
+            for x in self.hosts:
+                proc = processes[x['name']]
 
                 out, err = proc.communicate()
                 errCode = proc.wait()
                 if errCode != 0:
-                    res[x.name] = 'E:' + str(errCode)
+                    res[x['name']] = 'Error'
                     continue
 
                 found = re.search(pattern, out)
                 if not found:
-                    res[x.name] = '?'
+                    res[x['name']] = '?'
                     err('Ping regexp not found in %s' % out.replace('\n', '\\n'))
                     continue
 
-                res[x.name] = found.group(1)
+                res[x['name']] = found.group(1)
 
             with self.lock:
                 self.resp = res
-        except:
+        except Exception, e:
             with self.lock:
-                self.resp = {"Error":""}
+                self.resp = {"Error":e.message}
 
 _ping = _Ping()
