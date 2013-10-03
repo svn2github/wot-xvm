366fe65602f4ea088d178780640ee39364fd0c9e
pavlushko.m@gmail.com <pavlushko.m@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2013-10-03 16:26:30 +0000

    fix nightly
    git-svn-id: http://wot-xvm.googlecode.com/svn@2754 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/utils/build-system-linux/sh/xvm-swf-patch-as3.sh
+++ trunk/utils/build-system-linux/sh/xvm-swf-patch-as3.sh
@@ -11,20 +11,19 @@ files=(
 pushd ../../../src/xvm/swf/ > /dev/null
 export PATH=$PATH:../../../utils/build-system-linux/bin/rabcdasm/
 
-cp flash/*.swf ./*.orig.swf
+cp flash/* ./
 
 for (( i=0; i<${#files[@]}; i++ ));
   do
-    abcexport ${files[$i]}.orig.swf
-    rabcdasm ${files[$i]}.orig-0.abc
+    abcexport ${files[$i]}.swf
+    rabcdasm ${files[$i]}-0.abc
     patch --binary -p0 -s < ${files[$i]}.patch
-    rabcasm ${files[$i]}.orig-0/${files[$i]}.orig-0.main.asasm
-    abcreplace ${files[$i]}.orig.swf 0 ${files[$i]}.orig-0/${files[$i]}.orig-0.main.abc
-    rm ${files[$i]}.orig-0.abc
-    rm -rf ${files[$i]}.orig-0
+    rabcasm ${files[$i]}-0/${files[$i]}-0.main.asasm
+    abcreplace ${files[$i]}.swf 0 ${files[$i]}-0/${files[$i]}-0.main.abc
+    rm ${files[$i]}-0.abc
+    rm -rf ${files[$i]}-0
   done
 
-mv -f ./*.orig.swf ./*.swf
 mkdir -p ../../../bin/as3
 cp -f *.swf ../../../bin/as3
 
