bbb6e5fe414c2ba3c7831bdd2de76232f59edf62
pavlushko.m@gmail.com <pavlushko.m@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2014-01-24 14:17:50 +0000

    asdoc: initial support
    git-svn-id: http://wot-xvm.googlecode.com/svn@3173 802240b0-0b4d-18ce-4811-9324b01ec0ff

---
+++ trunk/utils/build-system-linux/build-asdoc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#XVM ASDoc builder for Linux
+#Part of XVM build system
+#Do not change anything in this file if you are not sure
+
+#1. Detect Flex version
+pushd config/ > /dev/null
+flex_version=$(cat flex_version)
+popd > /dev/null
+
+#2. Define Flex SDK path
+export FLEX_HOME="/opt/apache-flex-$flex_version"
+export PLAYERGLOBAL_HOME="$FLEX_HOME/frameworks/libs/player"
+export PATH="$PATH:$FLEX_HOME/bin"
+
+#3. Build swc
+pushd ../../src/xvm > /dev/null
+./build-swc-wg.sh
+popd > /dev/null
+
+#4. Build doc
+asdoc -source-path ../../src/xvm/src -doc-sources ../../src/xvm/src -library-path+="$PLAYERGLOBAL_HOME/10.2/playerglobal.swc" -library-path+=../../src/xvm/lib -output ../../doc
+
+#5. del lib folder
+rm -rf ../../src/xvm/lib
