1fcb970460e08d04ea21a9b1a759c4880118a15d
pavlushko.m@gmail.com <pavlushko.m@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2014-01-24 20:57:37 +0000

    asdoc: del previous doc before generation
    git-svn-id: http://wot-xvm.googlecode.com/svn@3180 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/utils/build-system-linux/build-asdoc.sh
+++ trunk/utils/build-system-linux/build-asdoc.sh
@@ -14,13 +14,16 @@ export FLEX_HOME="/opt/apache-flex-$flex_version"
 export PLAYERGLOBAL_HOME="$FLEX_HOME/frameworks/libs/player"
 export PATH="$PATH:$FLEX_HOME/bin"
 
-#3. Build swc
+#3. del content of doc folder
+rm -rf ../../doc/*
+
+#4. Build swc
 pushd ../../src/xvm > /dev/null
 ./build-swc-wg.sh
 popd > /dev/null
 
-#4. Build doc
+#5. Build doc
 asdoc -source-path ../../src/xvm/src -doc-sources ../../src/xvm/src -library-path+="$PLAYERGLOBAL_HOME/10.2/playerglobal.swc" -library-path+=../../src/xvm/lib -output ../../doc
 
-#5. del lib folder
+#6. del lib folder
 rm -rf ../../src/xvm/lib/
