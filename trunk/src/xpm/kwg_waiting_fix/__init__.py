d24ebf935d83feba07d452f6beddece26bed0aa3
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2014-02-11 14:41:59 +0000

    kwg_waiting_fix: update
    git-svn-id: http://wot-xvm.googlecode.com/svn@3207 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- trunk/src/xpm/kwg_waiting_fix/__init__.py
+++ trunk/src/xpm/kwg_waiting_fix/__init__.py
@@ -3,7 +3,7 @@
 #####################################################################
 # MOD INFO (mandatory)
 
-XPM_MOD_VERSION    = "0.1"
+XPM_MOD_VERSION    = "0.2"
 XPM_MOD_URL        = "http://www.koreanrandom.com/forum/topic/11630-/#entry151768"
 XPM_MOD_UPDATE_URL = ""
 XPM_GAME_VERSIONS  = ["0.8.11"]
@@ -12,15 +12,16 @@ XPM_GAME_VERSIONS  = ["0.8.11"]
 
 from gui.mods.xpm import *
 
-def WaitingView_close(base, self):
-    #log('WaitingView_close')
+def WaitingViewMeta_fix(base, self, *args):
     try:
-        base(self)
-    except:
-        logtrace(__file__)
+        base(self, *args)
+        #raise Exception('Test')
+    except Exception, ex:
+        log('[XPM] kwg_waiting_fix: %s throwed exception: %s' % (base.__name__, ex.message))
 
 def _Register():
-    from gui.Scaleform.framework.WaitingView import WaitingView
-    OverrideMethod(WaitingView, 'close', WaitingView_close)
+    from gui.Scaleform.daapi.view.meta.WaitingViewMeta import WaitingViewMeta
+    OverrideMethod(WaitingViewMeta, 'showS', WaitingViewMeta_fix)
+    OverrideMethod(WaitingViewMeta, 'hideS', WaitingViewMeta_fix)
 
 BigWorld.callback(0.001, _Register)
