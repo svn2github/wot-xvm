6917af173fdebc1085b22656cc178717db7f6259
m.schedriviy@gmail.com <m.schedriviy@gmail.com@802240b0-0b4d-18ce-4811-9324b01ec0ff>
2014-04-03 14:27:00 +0000

    change db engine from shelve to TinyDB (incomplete)
    git-svn-id: http://wot-xvm.googlecode.com/svn@3302 802240b0-0b4d-18ce-4811-9324b01ec0ff

--- branches/xvm-5.1.0-0.8.11/src/xpm/xvmstat/utils.py
+++ branches/xvm-5.1.0-0.8.11/src/xpm/xvmstat/utils.py
@@ -1,5 +1,11 @@
 """ xvm (c) sirmax 2013-2014 """
 
+import os
+
 def is_replay():
     import BattleReplay
     return BattleReplay.g_replayCtrl.isPlaying
+
+def rm(filename):
+    if os.path.isfile(filename):
+        os.remove(filename)
